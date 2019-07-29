8311517aeead49a52021ef3d2f580c3e47c18884
John Oncher <johnoncher@gmail.com>
2019-07-29 14:48:10 -0400

    added comments and 2nd image

--- src/App.js
+++ src/App.js
@@ -2,43 +2,52 @@ import React, { Component } from "react";
 import { Parallax } from "react-parallax";
 import { Container } from "semantic-ui-react";
 
+//This allows you to style the inner header div
 const insideStyles = {
-  background: "rgb(128,128,128,0.3)",
+  //header color
+  background: "rgb(255,255,255)",
   padding: 20,
   position: "absolute",
+  //spacing on the screen
   top: "45%",
   left: "50%",
   transform: "translate(-50%,-50%)"
 };
 export default class App extends Component {
   render() {
-    console.log(this.props.image);
     return (
-      <div id={"background"}>
+      <div>
+        {/*Spacer*/}
+        <Container style={{ height: window.innerHeight }}>
+          <div id={"header"} style={insideStyles}>
+            <h1 id={"header"}>{"Blank Space"}</h1>
+          </div>
+        </Container>
+        {/*Image 1*/}
         <Parallax
           bgImage={require("./photos/daylight-environment-forest-459225.jpg")}
           strength={750}
         >
           <Container style={{ height: window.innerHeight }}>
             <div id={"header"} style={insideStyles}>
-              <h1 id={"header"}>{"Welcome to PCC"}</h1>
+              <h1 id={"header"}>{"Image 1"}</h1>
             </div>
           </Container>
         </Parallax>
-        <Parallax strength={500}>
-          <Container style={{ height: window.innerHeight }}>
-            <div id={"header"} style={insideStyles}>
-              <h1 id={"header"}>{"Welcome to PCC"}</h1>
-            </div>
-          </Container>
-        </Parallax>
+        {/*Spacer*/}
+        <Container style={{ height: window.innerHeight }}>
+          <div id={"header"} style={insideStyles}>
+            <h1 id={"header"}>{"Blank Space"}</h1>
+          </div>
+        </Container>
+        {/*Image 2*/}
         <Parallax
-          bgImage={require("./photos/daylight-environment-forest-459225.jpg")}
+          bgImage={require("./photos/asphalt-dark-dawn-531321.jpg")}
           strength={1500}
         >
           <Container style={{ height: window.innerHeight }}>
             <div id={"header"} style={insideStyles}>
-              <h1 id={"header"}>{"Welcome to PCC"}</h1>
+              <h1 id={"header"}>{"Image 2"}</h1>
             </div>
           </Container>
         </Parallax>
